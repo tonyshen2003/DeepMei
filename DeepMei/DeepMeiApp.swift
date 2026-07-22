a46eeb8938052008dda64f07c7b0e83f93552bfc
Shen Sunfeng <shensunfeng@example.com>
2026-07-23 06:04:40 +0800

    Initial Commit

---
+++ DeepMei/DeepMeiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepMeiApp.swift
+//  DeepMei
+//
+//  Created by 沈孙丰 on 2026/7/23.
+//
+
+import SwiftUI
+
+@main
+struct DeepMeiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
