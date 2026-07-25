6ac317d503036ad9368e96a604cf2e9676dba5c9
Shen Sunfeng <shensunfeng@example.com>
2026-07-26 02:31:33 +0800

    refactor: 合并重复的NFCReader文件并清理冗余空行
    删除了冗余的NFCReader 2.swift文件，将代码统一保留在NFCReader.swift中，同时移除了文件顶部多余的空行

--- NFCReader.swift
+++ NFCReader.swift
@@ -1,4 +1,3 @@
-
 //
 //  Untitled.swift
 //  DeepMei
@@ -6,3 +5,80 @@
 //  Created by 沈孙丰 on 2026/7/23.
 //
 
+import Foundation
+import CoreNFC
+
+
+class NFCReader: NSObject, ObservableObject {
+
+    var session: NFCNDEFReaderSession?
+
+
+    func startScanning() {
+
+        guard NFCNDEFReaderSession.readingAvailable else {
+            print("设备不支持NFC")
+            return
+        }
+
+
+        session = NFCNDEFReaderSession(
+            delegate: self,
+            queue: DispatchQueue.main
+        )
+
+        session?.alertMessage = "请靠近社员卡"
+
+        session?.begin()
+    }
+}
+
+
+extension NFCReader: NFCNDEFReaderSessionDelegate {
+
+
+    func readerSessionDidBecomeActive(
+        _ session: NFCNDEFReaderSession
+    ) {
+
+        print("NFC开始扫描")
+
+    }
+
+
+
+    func readerSession(
+        _ session: NFCNDEFReaderSession,
+        didDetectNDEFs messages: [NFCNDEFMessage]
+    ) {
+
+        print("读取成功")
+
+        for message in messages {
+
+            for record in message.records {
+
+                let data = record.payload
+
+                print(data)
+
+            }
+
+        }
+
+    }
+
+
+    func readerSession(
+        _ session: NFCNDEFReaderSession,
+        didInvalidateWithError error: Error
+    ) {
+
+        print(
+            "NFC结束:",
+            error.localizedDescription
+        )
+
+    }
+
+}
